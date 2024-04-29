60c7508defa2cf6cffe78e978a1128d7e2f852ab
Pedro Alarcon <pedroa54@icloud.com>
2024-04-29 12:41:21 -0500

    practice complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
-#!/usr/bin/env python3
+def greet_programmer(name="programmer"):
+    print(f"Hello, {name}!")
 
-def greet_programmer():
-    pass
 
 def greet(name):
-    pass
+    print("Hello, {}!".format(name))
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, {}!".format(name))
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    return number / 2
